ffa953ea8205a711a9317b1d3df4928addc80342
Andrey Bataev <bataevai@gmail.com>
2023-02-20 21:27:21 +0400

    Exc.2

---
+++ Exc.2/Exc.2.py
@@ -0,0 +1,28 @@
+# Задача 2: Найдите сумму цифр трехзначного числа.
+# *Пример:*
+
+# 123 -> 6 (1 + 2 + 3)
+# 100 -> 1 (1 + 0 + 0) |
+import random
+number = random.randrange(100, 1000)
+
+def SumOfNumbers (num):
+    sum = 0
+    
+        # Простой вариант решения задачи
+        # sum = sum + num % 10 + (num // 10) % 10 + (num // 100) % 10
+
+        # Более полный вариант
+    while num > 0:
+        x = num % 10
+        sum = sum + x
+        num = num // 10
+    return sum  
+           
+
+print('Искомое число равно:')
+print(number)
+sumOfNum = SumOfNumbers(number)
+print("Сумма чисел равна:")
+print(sumOfNum)
+
